5e111da03ddd43e758ed2e83f23fad38df2d49d8
Max Winokan <max.winokan@lilly.com>
2026-03-06 16:29:49 +0000

    reformat

--- zsh.sh
+++ zsh.sh
@@ -1,4 +1,28 @@
 
 setopt PROMPT_SUBST
-PS1='%B%(?.%K{white}%F{black}.%K{red}%F{black}) %~ %k%f%b '
-RPROMPT='%B$CONDA_PROMPT_MODIFIER%b'
+
+__set_ps1() {
+  local RST='%b%k%f'
+  local CYAN_BG='%K{cyan}%F{black}%B'
+  local WHITE_BG='%K{white}%F{black}%B'
+  local RED_BG='%K{red}%F{black}%B'
+  local MAGENTA_BG='%K{magenta}%F{black}%B'
+  local BLUE_BG='%K{blue}%F{black}%B'
+
+  local COLOR="$WHITE_BG"
+  [[ $LAST_EXIT_CODE -ne 0 ]] && COLOR="$RED_BG"
+
+  local SCREEN_PROMPT_MODIFIER=""
+  [[ -n "$STY" ]] && SCREEN_PROMPT_MODIFIER="${MAGENTA_BG} ${STY#*.} ${RST} "
+
+  local CUSTOM_CONDA_MODIFIER=""
+  if [[ -n "$CONDA_PROMPT_MODIFIER" ]]; then
+    local env_name="${CONDA_PROMPT_MODIFIER//[()]/}"
+    env_name="${env_name// /}"
+    CUSTOM_CONDA_MODIFIER="${CYAN_BG} ${env_name} ${RST} "
+  fi
+
+  PS1="${CUSTOM_CONDA_MODIFIER}${SCREEN_PROMPT_MODIFIER}${COLOR} %~ ${RST} "
+}
+
+precmd() { LAST_EXIT_CODE=$? ; __set_ps1 }
